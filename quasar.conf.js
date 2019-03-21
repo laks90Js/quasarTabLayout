16dc43ed2d1aaa5c5d14ffc2e8726366d9497e68
lakshmi sachidanandam <lakshmi.sachidanandam@sap.com>
2019-03-21 14:34:24 +0530

    Adding tabs + data

--- quasar.conf.js
+++ quasar.conf.js
@@ -53,7 +53,11 @@ module.exports = function (ctx) {
         'QListHeader',
         'QItem',
         'QItemMain',
-        'QItemSide'
+        'QItemSide',
+        'QTabs',
+        'QTab',
+        'QTabPane',
+        'QRouteTab'
       ],
       directives: [
         'Ripple'
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,15 @@
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+  <q-page class="flex">
+    <q-tabs>
+  <!-- Tabs - notice slot="title" -->
+  <q-tab default slot="title" name="tab-1" icon="message" />
+  <q-tab slot="title" name="tab-2" icon="fingerprint" />
+  <q-tab slot="title" name="tab-3" icon="account_box" />
+  <!-- Targets -->
+  <q-tab-pane name="tab-1">Tab One</q-tab-pane>
+  <q-tab-pane name="tab-2">Tab Two</q-tab-pane>
+  <q-tab-pane name="tab-3">Tab Three</q-tab-pane>
+</q-tabs>
   </q-page>
 </template>
 
@@ -8,7 +17,31 @@
 </style>
 
 <script>
+import axios from 'axios';
+
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  methods : {
+    loadData()  {
+      axios.get("../assets/tabs.json")
+      .then(reponse => {
+        console.log(response);
+      }).catch(error=>{
+        console.log(error)
+      })
+    },
+    mounted () {
+      this.loadData()
+    }
+  }
 }
 </script>
+
+
+
+
+export default ({app, router, Vue}) => {
+  Vue.prototype.$axios = axios
+  // ^ ^ ^ this will allow you to use this.$axios
+  //       so you won't necessarily have to import axios in each vue file
+}
